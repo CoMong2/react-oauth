29b2419b57804aab7db15fba4934b82f6414c90d
CoMong2 <85171864+CoMong2@users.noreply.github.com>
2023-03-31 08:30:34 +0900

    react-oauth init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+/** eslint-disable */
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [uri, setUri] = useState('');
+
+  useEffect(() => {
+    axios.get(uri).then((response) => {
+      setMessage(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setUri('/auth/google')}>Google</button>
+      {/* <h1>{message}</h1> */}
     </div>
   );
 }
