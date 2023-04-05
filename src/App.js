17b681c1b3e75b0674df1687de401f4e5287fd53
CoMong2 <85171864+CoMong2@users.noreply.github.com>
2023-04-05 14:59:47 +0900

    output user info from cookie

--- src/App.js
+++ src/App.js
@@ -15,48 +15,9 @@ const App = () => {
         <Route path={'callback/error'} element={<LoginErrorCallback/>} />
       </Route>
 
-      <Route path='/logout'></Route>
-
       {/* <Route path='*' element={<NotFound />} /> */}
     </Routes>
   );
 }
 
 export default App;
-
-
-// function App() {
-//   const [result, setResult] = useState(null);
-
-//   const moveToAuth = async () => {
-    
-
-//     try {
-//       // axios.get(process.env.REACT_APP_API_PATH + '/auth/google')
-//       //   .then((result) => {
-//       //     console.log(result.data);
-//       //   }).catch((error) => {
-//       //     console.log(error);
-//       //   });
-//       window.location.href=process.env.REACT_APP_API_PATH + '/auth/google';
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   return (
-//     <div className="App">
-//       <div>
-//         <h1>Minions</h1>
-//         <span onClick={() => {moveToAuth();}}>
-//           <img src={ google } />
-//         </span>
-//       </div>
-//       <div>
-//         {/* <GoogleAuthButton/> */}
-//       </div>
-//     </div>
-//   );
-// }
-  
-// export default App;
